c0f375deafbdc95b17611c6b40b9f6127b2a41a5
DamjanKolevski <damjankolevski9@gamil.com>
2023-12-05 18:20:19 +0100

    first commit

---
+++ src/zadacaa/ite.java
@@ -0,0 +1,22 @@
+java import.util
+public static void main(string []args) {
+		int i,n;
+		System.out.println("Do brojot");
+		Scanner input;
+		n= input.nextInt();
+		i=1;
+		while (i <= n)
+		{
+			if (i%5 == 0)
+        {
+		System.out.println(n);
+		}
+			i=i+1;
+			
+		}
+		
+	}
+
+}
+
+
